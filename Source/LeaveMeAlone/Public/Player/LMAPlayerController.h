18565f6c7eeb3ac93a7d33340d5bd54c3c237b63
PleNik <pleshkovn69@gmail.com>
2025-02-08 11:06:03 +0500

    LMAPlayerController_Cpp Created

---
+++ Source/LeaveMeAlone/Public/Player/LMAPlayerController.h
@@ -0,0 +1,14 @@
+//  LeaveMeAlone Game by Netologiya. All RightsReserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LMAPlayerController.generated.h"
+
+UCLASS()
+class LEAVEMEALONE_API ALMAPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
