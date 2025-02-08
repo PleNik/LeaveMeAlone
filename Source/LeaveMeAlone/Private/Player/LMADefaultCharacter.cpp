fa2ee980149cc1f50bc0ebb68cf41065f2064d17
PleNik <pleshkovn69@gmail.com>
2025-02-08 10:51:31 +0500

    Project is initialized

---
+++ Source/LeaveMeAlone/Private/Player/LMADefaultCharacter.cpp
@@ -0,0 +1,28 @@
+//  LeaveMeAlone Game by Netologiya. All RightsReserved.
+
+#include "Player/LMADefaultCharacter.h"
+
+ALMADefaultCharacter::ALMADefaultCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ALMADefaultCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ALMADefaultCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ALMADefaultCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/LeaveMeAlone/Public/Player/LMADefaultCharacter.h
@@ -0,0 +1,25 @@
+//  LeaveMeAlone Game by Netologiya. All RightsReserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "LMADefaultCharacter.generated.h"
+
+UCLASS()
+class LEAVEMEALONE_API ALMADefaultCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ALMADefaultCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
