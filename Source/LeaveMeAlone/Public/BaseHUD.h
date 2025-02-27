f76775cf8f8e6268fb9bf66d4c4ba8bc72efda89
PleNik <pleshkovn69@gmail.com>
2025-02-27 21:32:13 +0500

    Added HealthBar

---
+++ Source/LeaveMeAlone/Public/BaseHUD.h
@@ -0,0 +1,20 @@
+//  LeaveMeAlone Game by Netologiya. All RightsReserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "BaseHUD.generated.h"
+
+class UUserWidget;
+
+UCLASS()
+class LEAVEMEALONE_API ABaseHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+    UPROPERTY(BlueprintReadWrite)
+	TArray<UUserWidget*> WidgetsContainer;
+	
+};
